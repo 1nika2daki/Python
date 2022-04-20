cf5666f21642464c2260c04d469bb15965287c70
uh-ng <1nika2girgvliani@gmail.com>
2022-04-20 22:29:22 +0400

    stepick last task 2.3 done

---
+++ stepick_2.3-last_task.py
@@ -0,0 +1,25 @@
+# put your python code here
+a=input()
+
+a=a.upper()
+
+
+a=a.replace(A, )
+a=a.replace(O, )
+a=a.replace(Y, )
+a=a.replace(E, )
+a=a.replace(U, )
+a=a.replace(I, )
+k=[]
+l='.'
+a=a.replace( ,)
+
+
+for i in a:
+    k.append(l)
+    k.append(i)
+print(''.join(k).lower())
+
+
+
+
